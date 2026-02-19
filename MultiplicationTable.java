4798fd60cbddee145a789d02da57d3ddf4d7d8fa
Ankit Ydv <ankityadav11241@gmail.com>
2026-02-19 12:07:23 +0530

    multiplication table

---
+++ MultiplicationTable.java
@@ -0,0 +1,7 @@
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.printf("5 X %d = %d\n", i, 5 * i);
+        }
+    }
+}
